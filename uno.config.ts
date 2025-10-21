18ec1b53672562679ba2368927ea11d4fd9b22ca
Jerry Bruwes <jbruwes@icloud.com>
2025-10-21 13:06:17 +0200

    feat: add pug extractor and define config to uno config

--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,17 @@
+import extractorPug from "@unocss/extractor-pug";
 import attributify from "@unocss/preset-attributify";
 import icons from "@unocss/preset-icons/browser";
 import tagify from "@unocss/preset-tagify";
 import typography from "@unocss/preset-typography";
 import wind4 from "@unocss/preset-wind4";
+import { defineConfig } from "@unocss/runtime";
 
-export default {
+/* -------------------------------------------------------------------------- */
+/*                        Настройки UnoCSS для проекта                        */
+/* -------------------------------------------------------------------------- */
+
+export default defineConfig({
+  extractors: [extractorPug()],
   presets: [
     wind4({ preflights: { reset: true } }),
     typography(),
@@ -12,4 +19,4 @@ export default {
     tagify(),
     attributify(),
   ],
-};
+});
