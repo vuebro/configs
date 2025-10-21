b0c07e15a7ea180a289934b5509e3679eaa676ed
Jerry Bruwes <jbruwes@icloud.com>
2025-10-21 13:05:05 +0200

    Add UnoCSS plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
+import UnoCSS from "@unocss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
+
+/* -------------------------------------------------------------------------- */
+/*                         Настройки vite для проекта                         */
+/* -------------------------------------------------------------------------- */
 
 export default defineConfig({
   base: "./",
   define: {
     __APP_VERSION__: JSON.stringify(process.env["npm_package_version"]),
   },
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), UnoCSS()],
   resolve: { alias: { "@": "./src" } },
 });
