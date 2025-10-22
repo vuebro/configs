92f44b67a92ab131759d90279214b7895aa68642
Jerry Bruwes <jbruwes@icloud.com>
2025-10-22 13:24:23 +0200

    refactor: update UnoCSS config with new transformers and extractor

--- uno.config.ts
+++ uno.config.ts
@@ -1,22 +1,21 @@
+import { extractorArbitraryVariants } from "@unocss/extractor-arbitrary-variants";
 import extractorPug from "@unocss/extractor-pug";
-import attributify from "@unocss/preset-attributify";
-import icons from "@unocss/preset-icons/browser";
-import tagify from "@unocss/preset-tagify";
-import typography from "@unocss/preset-typography";
-import wind4 from "@unocss/preset-wind4";
-import { defineConfig } from "@unocss/runtime";
+import {
+  defineConfig,
+  transformerCompileClass,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
 
 /* -------------------------------------------------------------------------- */
 /*                        Настройки UnoCSS для проекта                        */
 /* -------------------------------------------------------------------------- */
 
 export default defineConfig({
-  extractors: [extractorPug()],
-  presets: [
-    wind4({ preflights: { reset: true } }),
-    typography(),
-    icons({ cdn: "https://cdn.jsdelivr.net/npm/" }),
-    tagify(),
-    attributify(),
+  extractors: [extractorPug(), extractorArbitraryVariants()],
+  transformers: [
+    transformerVariantGroup(),
+    transformerDirectives(),
+    transformerCompileClass(),
   ],
 });
