f48f9cab098b1c1b09ed4f1eb39236b34efc7640
Jerry Bruwes <jbruwes@icloud.com>
2025-10-22 13:24:36 +0200

    Add UnoCSS presets configuration

---
+++ uno.presets.ts
@@ -0,0 +1,40 @@
+import type { AttributifyOptions } from "@unocss/preset-attributify";
+import type { IconsOptions } from "@unocss/preset-icons/browser";
+import type { TagifyOptions } from "@unocss/preset-tagify";
+import type { TypographyOptions } from "@unocss/preset-typography";
+import type { WebFontsOptions } from "@unocss/preset-web-fonts";
+import type { PresetWind4Options } from "@unocss/preset-wind4";
+
+import attributify from "@unocss/preset-attributify";
+import icons from "@unocss/preset-icons/browser";
+import tagify from "@unocss/preset-tagify";
+import typography from "@unocss/preset-typography";
+import webFonts from "@unocss/preset-web-fonts";
+import wind4 from "@unocss/preset-wind4";
+
+/* -------------------------------------------------------------------------- */
+/*                         Пресеты UnoCSS для проекта                         */
+/* -------------------------------------------------------------------------- */
+
+export default ({
+  attributifyOptions,
+  iconsOptions = { cdn: "https://cdn.jsdelivr.net/npm/" },
+  tagifyOptions,
+  typographyOptions,
+  webFontsOptions,
+  wind4Options = { preflights: { reset: true } },
+}: {
+  attributifyOptions?: AttributifyOptions;
+  iconsOptions?: IconsOptions;
+  tagifyOptions?: TagifyOptions;
+  typographyOptions?: TypographyOptions;
+  webFontsOptions?: WebFontsOptions;
+  wind4Options?: PresetWind4Options;
+} = {}) => [
+  attributify(attributifyOptions),
+  icons(iconsOptions),
+  tagify(tagifyOptions),
+  typography(typographyOptions),
+  webFonts(webFontsOptions),
+  wind4(wind4Options),
+];
