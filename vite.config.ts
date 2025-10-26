3d6d4cde4b756bec0e0cd2e739808d3e855ad8d2
Jerry Bruwes <jbruwes@icloud.com>
2025-10-26 12:23:33 +0200

    Add path alias and reorganize vite config structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import vueDevTools from "vite-plugin-vue-devtools";
+// import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import unoCSS from "@unocss/vite";
 
-/* -------------------------------------------------------------------------- */
-/*                         Настройки vite для проекта                         */
-/* -------------------------------------------------------------------------- */
-
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "./src",
+      // fileURLToPath(new URL("./src", import.meta.url))
+    },
+  },
   define: {
     __APP_VERSION__: JSON.stringify(process.env["npm_package_version"]),
   },
   plugins: [vue(), vueDevTools(), unoCSS()],
-  resolve: { alias: { "@": "./src" } },
-  base: "./",
 });
