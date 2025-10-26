c802d85ff4973ecf5ae31e6542d12c9cdca6a71f
Jerry Bruwes <jbruwes@icloud.com>
2025-10-26 13:08:46 +0200

    feat: add vite-tsconfig-paths for TypeScript path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import vueDevTools from "vite-plugin-vue-devtools";
-import { fileURLToPath, URL } from "node:url";
+import tsconfigPaths from "vite-tsconfig-paths";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import unoCSS from "@unocss/vite";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
   define: {
     __APP_VERSION__: JSON.stringify(process.env["npm_package_version"]),
   },
-  plugins: [vue(), vueDevTools(), unoCSS()],
-  base: "",
+  plugins: [vue(), vueDevTools(), unoCSS(), tsconfigPaths()],
 });
